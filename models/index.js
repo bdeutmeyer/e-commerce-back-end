5fca8707013aa9ebfbfc3fdda305db15e0df3bb1
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-27 09:11:40 -0500

    all working! will drop and reseed db before demo

--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ Product.belongsToMany(Tag, {
   model: ProductTag,
   unique: false,
   },
-  as: 'product_tag_info'
+  as: 'product_tag_info',
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -27,7 +27,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: 'tag_product_info'
+  as: 'tag_product_info',
 })
 
 module.exports = {
